3d26081419031ba10cf51de8e698e560bd5ec850
frodri1 <frodri1@PC00907949.ad.esi.adp.com>
2015-11-11 12:05:20 +0100

    A new string was added

---
+++ src/main/java/ADP/codeutils/CommonData.java
@@ -0,0 +1,16 @@
+package ADP.codeutils;
+
+/**
+ *
+ * @author FernandoRodriguez
+ */
+public class CommonData {
+  
+  public final static String Production = "http://www.skyscanner.es";
+  public final static String Test = "http://www.test.skyscanner.local";
+  
+  public final static String hsdDate = "29%2F08%2F2014";
+  public final static String sdDate = "2014-08-29";
+  public final static String hedDate = "30%2F08%2F2014";
+  public final static String edDate = "2014-08-30";
+}
